0106a0c7cb123d79d20a219b1ea3a9d6a6357b45
Ruben Silvera <rub-silvera@hotmail.com>
2024-08-10 14:59:42 -0300

    correcciones en botons

--- Proyecto_React/src/components/ItemDetail.jsx
+++ Proyecto_React/src/components/ItemDetail.jsx
@@ -1,13 +1,16 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import ItemCount from "./ItemCount";
 import { CartContext } from "./context/CartContext";
+import { Link } from "react-router-dom";
 
 const ItemDetail = ({ item }) => {
-    const {addItem} = useContext(CartContext);
+    const { addItem } = useContext(CartContext);
+    const [purchaseCompleted, setPurchaseCompleted] = useState(false);
 
     const onAdd = (quantity) => {
         addItem(item, quantity);
-    }
+        setPurchaseCompleted(true);
+    };
 
     return (
         <div className="bomber-section">
@@ -21,6 +24,13 @@ const ItemDetail = ({ item }) => {
                     <p>Precio: ${item.price}</p>
                     <p>Stock disponible: {item.stock}</p>
                     <ItemCount stock={item.stock} onAdd={onAdd} />
+                    {purchaseCompleted && (
+                        <div className="d-flex flex-column gap-2 mt-3">
+                            <Link to="/" className="btn" style={{ backgroundColor: 'white', color: 'red', border: '1px solid red' }}>
+                                Seguir comprando
+                            </Link>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
